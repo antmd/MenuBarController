9f4f1f4b17fb5ec5408513ea4e1f9431435de63f
Dmitry Nikolaev <blackliteon@gmail.com>
2014-10-27 14:25:45 +0300

    Initial Commit

---
+++ MenuBarController/MenuBarController.h
@@ -0,0 +1,19 @@
+//
+//  MenuBarController.h
+//  MenuBarController
+//
+//  Created by Dmitry Nikolaev on 27.10.14.
+//  Copyright (c) 2014 Dmitry Nikolaev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MenuBarController.
+FOUNDATION_EXPORT double MenuBarControllerVersionNumber;
+
+//! Project version string for MenuBarController.
+FOUNDATION_EXPORT const unsigned char MenuBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MenuBarController/PublicHeader.h>
+
+
